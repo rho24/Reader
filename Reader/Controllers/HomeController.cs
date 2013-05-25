d1e7d56b48a5e8be9db48bdc94cdbb48ed126989
Richard Oliver <richard@rholiver.co.uk>
2013-05-25 23:08:57 +0100

    Initial skin

--- Reader/Controllers/HomeController.cs
+++ Reader/Controllers/HomeController.cs
@@ -15,6 +15,5 @@ namespace Reader.Controllers
         {
             return View();
         }
-
     }
 }
---
+++ Reader/Scripts/site.js
@@ -0,0 +1,10 @@
+﻿$(function() {
+    // Fluid layout doesn't seem to support 100% height; manually set it
+    $(window).resize(function () {
+        var viewportHeight = $(window).height() - $(".top-navbar").height() - 100;
+        
+        $('.viewport').height(viewportHeight);
+    });
+    $(window).resize();
+
+});
