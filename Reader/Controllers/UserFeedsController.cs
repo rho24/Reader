df1f3f0044e7dd30a14f761c6d733e70817bfbb5
Richard Oliver <richard@rholiver.co.uk>
2013-05-26 16:33:02 +0100

    First api bits

---
+++ Reader/Controllers/UserFeedsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Reader.Controllers
+{
+    public class UserFeedsController : ApiController
+    {
+        // GET api/feeds
+        public IEnumerable<UserFeed> Get() {
+            return new[] {
+                new UserFeed {
+                    isGroup = true,
+                    name = "Forums",
+                    nodes = new[] {
+                        new UserFeed {name = "Glimpse"},
+                        new UserFeed {name = "Autofac"},
+                        new UserFeed {name = "RavenDb2"}
+                    }
+                },
+                new UserFeed {name = "Hanselman"},
+                new UserFeed {name = "Haacked"},
+                new UserFeed {name = "ScottGu2"}
+            };
+        }
+    }
+
+    #region Nested type: UserFeed
+
+    public class UserFeed
+    {
+        public bool isGroup { get; set; }
+
+        public string name { get; set; }
+
+        public IEnumerable<UserFeed> nodes { get; set; }
+    }
+
+    #endregion
+}
