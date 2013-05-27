0462f0bcfa220e4ae92cba77b8a05f3d841ee74b
Richard Oliver <richard@rholiver.co.uk>
2013-05-27 20:35:35 +0100

    Attached ravendb profiler

--- Reader/App_Start/AutofacConfig.cs
+++ Reader/App_Start/AutofacConfig.cs
@@ -34,6 +34,8 @@ namespace Reader.App_Start
 
             container.ActivateGlimpse();
 
+            Glimpse.RavenDb.Profiler.AttachTo(container.Resolve<IDocumentStore>() as DocumentStore);
+
             // Set the WebApi dependency resolver.
             configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
 
